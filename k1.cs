85a32c65a3f3d2aa6ab84d0d6afc0e9de0ecca0e
Shi Chao <shi@semgrep.com>
2026-01-21 09:45:16 +0800

    update

--- k1.cs
+++ k1.cs
@@ -7,9 +7,6 @@ class Program
         Console.WriteLine("Hello, World!");
         string AWS_ACCESS_KEY_ID="AKIA1234567890TESTKEY"; //FP due to length and keyword `TEST`
         string AWS_ACCESS_KEY_ID="AKIA123456789T1STKEY";  //TP
-        
-        string dbPassword="P@ssw0rdc123";
-
 
         string jwtSecret1= "super_secret_key_123456"; //FP as it is used anywhere
         var json = new JwtBuilder()
