123800768d42469cdea30ebfe8855f49c68f5909
Shi Chao <shi@semgrep.com>
2026-01-21 09:44:54 +0800

    update

--- k1.cs
+++ k1.cs
@@ -1,3 +1,19 @@
-         string awsKey = "AKIA1234567890TESTKEY";
-        string dbPassword = "P@ssw0rdc123";
-        string jwtSecret = "super_secret_key_123456";
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string AWS_ACCESS_KEY_ID="AKIA1234567890TESTKEY"; //FP due to length and keyword `TEST`
+        string AWS_ACCESS_KEY_ID="AKIA123456789T1STKEY";  //TP
+        
+        string dbPassword="P@ssw0rdc123";
+
+
+        string jwtSecret1= "super_secret_key_123456"; //FP as it is used anywhere
+        var json = new JwtBuilder()
+        .Decode<IDictionary<string, object>>(jwtToken)
+        .WithSecret("super_secret_key_123456");       // TP
+    }
+}
---
+++ k2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string dbPassword="P@ssw0rdc123";
+    }
+}
